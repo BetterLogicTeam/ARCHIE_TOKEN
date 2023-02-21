4fa19b4d5a5aa8966b77ab3cf56dc38f4d7cc9e6
betterlogics <108340548+betterlogics@users.noreply.github.com>
2023-02-21 16:31:20 +0500

    work nav_li and sliders

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,18 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Graph_part from './Components/Graph_part/Graph_part';
 import Home_card from './Components/Home_card/Home_card';
 import KittyMain from './Components/KittyMain/KittyMain';
+import Home_slider_1 from './Components/Home_slider_1/Home_slider_1';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-<Header/>
-<Graph_part/>
-<KittyMain/>
-{/* <Home_card/> */}
+      <Header />
+      <Graph_part />
+      <KittyMain />
+      <Home_slider_1/>
+      {/* <Home_card/> */}
     </div>
   )
 }
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -5,76 +5,91 @@ import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import H_logo from "../Assests/H_logo.png"
 import "./Header.css"
+import { AiFillInfoCircle } from "react-icons/ai";
+import { BsTelegram } from "react-icons/bs";
+import { BsTwitter } from "react-icons/bs";
+import { FaDiscord } from "react-icons/fa";
+import { BsInstagram } from "react-icons/bs";
+import { BsFacebook } from "react-icons/bs";
+import { BsReddit } from "react-icons/bs";
+import { BsYoutube } from "react-icons/bs";
+import { FaTiktok } from "react-icons/fa";
 
 export default function Header() {
   return (
     <div>
-         <Navbar collapseOnSelect expand="lg" className='main_bg_nav' variant="dark">
-      <Container>
-        <Navbar.Brand href="#home"><img src={H_logo} className='header_logo' alt="" /></Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="">
-            <Nav.Link href="#features" className='header_links'>Home</Nav.Link>
-            {/* <Nav.Link href="#pricing">Pricing</Nav.Link> */}
-            <NavDropdown  className='header_links' title="ECO-SYSTEM" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-            <NavDropdown  className='header_links' title="ARCHIE-NFT" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-            <NavDropdown className='header_links' title="DOCUMENTS" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-            <Nav.Link href="#features" className='header_links'>BUY $ARCHIE</Nav.Link>
-            <NavDropdown title="STACKING" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-            <Nav.Link href="#features" className='header_links'>WECAREKITTY</Nav.Link>
+      <Navbar collapseOnSelect expand="lg" className='main_bg_nav' variant="dark">
+        <Container className='ms-0'>
+          <Navbar.Brand href="#home"><img src={H_logo} className='header_logo' alt="" /></Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="">
+              <Nav.Link href="#features" className='header_links'>Home</Nav.Link>
+              <NavDropdown title={<span className='header_links'>ECO-SYSTEM</span>} id="collasible-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1" className='all_hvr'>Archie Chain (ARC)</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2" className='all_hvr'>Archie Swap</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3" className='all_hvr'>Archie Scan</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.4" className='all_hvr'>ArcCoin ($ARC)</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.5" className='all_hvr'>WarcCoin ($WARC)</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.6" className='all_hvr'>Archie Aggregaor</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.7" className='all_hvr'>ArchiePlace</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.8" className='all_hvr'>ArchieWallet</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.9" className='all_hvr'>ArchieMeta</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.10" className='all_hvr'>Island of Archie</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.11" className='all_hvr'>Play 2 Win</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.12" className='all_hvr'>Archie Ecommerce</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.13" className='all_hvr'>Archie Reality</NavDropdown.Item>
+              </NavDropdown>
 
+              <NavDropdown title={<span className='header_links'>ARCHIE-NFT</span>}id="collasible-nav-dropdown">
+                <NavDropdown.Item className='all_hvr' href="#action/3.1">Archieneko NFT</NavDropdown.Item>
+                <NavDropdown.Item className='all_hvr' href="#action/3.2">ArchieMeta NFT</NavDropdown.Item>
+                <NavDropdown.Item className='all_hvr' href="#action/3.3">We Care Kitty NFT</NavDropdown.Item>
+              </NavDropdown>
 
-          </Nav>
-          {/* <Nav>
+              <NavDropdown  title={<span className='header_links'>DOCUMENTS</span>} id="collasible-nav-dropdown">
+                <NavDropdown.Item className='all_hvr' href="#action/3.1">Token Allocations</NavDropdown.Item>
+                <NavDropdown.Item className='all_hvr' href="#action/3.2">Tokenomics</NavDropdown.Item>
+                <NavDropdown.Item className='all_hvr' href="#action/3.3">Road Map</NavDropdown.Item>
+                <NavDropdown.Item className='all_hvr' href="#action/3.4">Audit Report</NavDropdown.Item>
+                <NavDropdown.Item className='all_hvr' href="#action/3.5">White Paper</NavDropdown.Item>
+                <NavDropdown.Item className='all_hvr' href="#action/3.6">Team / KYC</NavDropdown.Item>
+              </NavDropdown>
+
+              <Nav.Link href="#features" className='header_link_by_arch'>BUY $ARCHIE</Nav.Link>
+
+              <NavDropdown title={<span className='header_links'>STACKING</span>} id="collasible-nav-dropdown">
+                <NavDropdown.Item className='all_hvr' href="#action/3.1">Archie Staking</NavDropdown.Item>
+                <NavDropdown.Item className='all_hvr' href="#action/3.2">NFT Staking</NavDropdown.Item>
+              </NavDropdown>
+
+              <Nav.Link href="#features" className='header_links'>WECAREKITTY</Nav.Link>
+
+              <NavDropdown  className='info_list' title={<AiFillInfoCircle className="info_icon mb-1"/>} id="collasible-nav-dropdown">
+                <NavDropdown.Item className='all_hvr text-center' href="#action/3.1"><BsTelegram className="icon_edt"/></NavDropdown.Item>
+                <NavDropdown.Item className='all_hvr text-center' href="#action/3.2"><BsTwitter className="icon_edt"/></NavDropdown.Item>
+                <NavDropdown.Item className='all_hvr text-center' href="#action/3.3"><FaDiscord className="icon_edt"/></NavDropdown.Item>
+                <NavDropdown.Item className='all_hvr text-center' href="#action/3.4"><BsInstagram className="icon_edt"/></NavDropdown.Item>
+                <NavDropdown.Item className='all_hvr text-center' href="#action/3.5"><BsFacebook className="icon_edt"/></NavDropdown.Item>
+                <NavDropdown.Item className='all_hvr text-center' href="#action/3.6"><FaTiktok className="icon_edt"/></NavDropdown.Item>
+                <NavDropdown.Item className='all_hvr text-center' href="#action/3.7"><BsReddit className="icon_edt"/></NavDropdown.Item>
+                <NavDropdown.Item className='all_hvr text-center' href="#action/3.8"><BsYoutube className="icon_edt"/></NavDropdown.Item>
+              </NavDropdown>
+
+              <NavDropdown  className='info_list_2' title={<span>DAPP</span>} id="collasible-nav-dropdown">
+                <NavDropdown.Item className='all_hvr' href="#action/3.1">comming soon</NavDropdown.Item>
+              </NavDropdown>
+
+            </Nav>
+            {/* <Nav>
             <Nav.Link href="#deets">More deets</Nav.Link>
             <Nav.Link eventKey={2} href="#memes">
               Dank memes
             </Nav.Link>
           </Nav> */}
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </div>
   )
 }
