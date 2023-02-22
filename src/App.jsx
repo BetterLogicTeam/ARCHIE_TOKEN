d2f77deffbec3a9fb726434605ab8155c8f73ba8
betterlogics <108340548+betterlogics@users.noreply.github.com>
2023-02-22 15:24:50 +0500

    road map

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,18 @@ import Graph_part from './Components/Graph_part/Graph_part';
 import Home_card from './Components/Home_card/Home_card';
 import KittyMain from './Components/KittyMain/KittyMain';
 import Home_slider_1 from './Components/Home_slider_1/Home_slider_1';
+import Road_map from './Components/Road_map/Road_map';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <Header />
-      <Graph_part />
-      <KittyMain />
-      <Home_slider_1/>
+      <Road_map/>
+      {/* <Header /> */}
+      {/* <Graph_part /> */}
+      {/* <KittyMain /> */}
+      {/* <Home_slider_1/> */}
       {/* <Home_card/> */}
     </div>
   )
